33602ed58f641414f7c245c703e6cfca4dfe02b8
potato0221 <wnqo10@naver.com>
2025-12-02 17:32:36 +0900

    feat: 프로젝트 셋팅

---
+++ src/main/java/com/ll/homescope/HomeScopeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.homescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeScopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/homescope/HomeScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.homescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
